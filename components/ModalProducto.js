034c8ab95ec1677e3131890519cf4a9421bb5d59
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-09-21 21:31:07 -0500

    Funcionalidad para ajustar las cantidades

--- components/ModalProducto.js
+++ components/ModalProducto.js
@@ -1,10 +1,14 @@
 import Image from "next/image"
 import useQuiosco from "../hooks/useQuiosco"
 import { formatearDinero } from "../helpers"
+import { useState } from "react"
 
 export const ModalProducto = () => {
 
   const { producto, handleChangeModal } = useQuiosco()
+
+  const [cantidad, setCantidad] = useState(1)
+
   return (
     <div className="md:flex gap-10">
       <div className='md:w-1/3'>
@@ -29,6 +33,31 @@ export const ModalProducto = () => {
         <p className="mt-5 font-black text-5xl text-amber-500">
           {formatearDinero(producto.precio)}
         </p>
+        <div className="flex gap-4 mt-5">
+          <button
+            type="button"
+            onClick={() => {
+              if(cantidad <= 1) return;
+              setCantidad(cantidad - 1)
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+          </button>
+          <p className="text-3xl">{cantidad}</p>
+          <button
+            type="button"
+            onClick={() => {
+              if(cantidad >= 5) return;
+              setCantidad(cantidad + 1)
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   )
