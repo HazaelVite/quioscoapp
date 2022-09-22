7db156af0fab2d9d3892dac3a255f42645a1eb24
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-09-21 21:09:51 -0500

    Detectar al producto que se le da click

--- components/Producto.js
+++ components/Producto.js
@@ -1,7 +1,10 @@
 import Image from "next/image"
 import { formatearDinero } from "../helpers"
+import useQuiosco from "../hooks/useQuiosco"
 
 const Producto = ({ producto }) => {
+  // Llamando el hook
+  const { handleSetProducto, handleChangeModal } = useQuiosco()
   const { nombre, imagen, precio } = producto
   return (
     <div className='border p-3'>
@@ -16,6 +19,15 @@ const Producto = ({ producto }) => {
         <p className="mt-5 font-black text-4xl text-amber-500">
           {formatearDinero(precio)}
         </p>
+
+        <button 
+        type="button"
+        className="bg-indigo-600 hover:bg-indigo-700 text-white w-full mt-5 p-3 uppercase font-bold"
+        onClick={() => {
+          handleChangeModal()
+          handleSetProducto(producto)
+        } }
+        >Agregar</button>
       </div>
     </div>
   )
