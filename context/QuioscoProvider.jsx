58b56d05f87ffea6465261a812fdf46a1b7bd8b4
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-09-23 20:49:50 -0500

    Llevar al usuario al menu principal

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, createContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 const QuioscoContext = createContext()
 
@@ -10,7 +11,8 @@ const QuioscoProvider = ({children}) => {
   const [producto, setProducto ] = useState({})
   const [modal, setModal] = useState(false)
   const [pedido, setPedido] = useState([])
-  const [paso, setPaso] = useState(1)
+  
+  const router = useRouter()
 
   const obtenerCategorias = async () => {
     const { data } = await axios('/api/categorias')
@@ -28,6 +30,7 @@ const QuioscoProvider = ({children}) => {
   const handleClickCategoria = id => {
     const categoria = categorias.filter(cat => cat.id === id)
     setCategoriaActual(categoria[0])
+    router.push('/')
   }
 
   // Funcion para setear el producto
