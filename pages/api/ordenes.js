4cbb7d98f41bfeaa5dd84d77bd3d45b34bd9d64a
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-09-26 21:39:58 -0500

    Insertando registros en la DB

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -1,8 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+
+
 export default async function handle(req, res) {
+  const prisma = new PrismaClient()
 
   if(req.method === "POST") {
-    console.log(req.body);
-    res.json({metodo: "POST"})
+    // Para crear el registro
+    const orden = await prisma.orden.create({
+      data: {
+        nombre: req.body.nombre,
+        total: req.body.total,
+        pedido: req.body.pedido,
+        fecha: req.body.fecha
+      }
+    })
+    // console.log(req.body);
+    // res.json({metodo: "POST"})
+    res.json(orden)
   }
 
 }
